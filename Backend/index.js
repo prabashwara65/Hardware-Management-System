cf88f14c440b2e828b74fd87657053eb3b052068
Ishara Umayangana <isharaumayangana27@gmail.com>
2024-02-19 23:06:32 +0530

    Revert "Merge pull request #14 from SanjukaRavishan/rentalManagement"
    This reverts commit 091731d57f744391ec4ea6f3110fef9f20636b33, reversing
    changes made to e1c1887c8088e6503667aa82e3507dfcaecb9c44.

--- Backend/index.js
+++ Backend/index.js
@@ -1,60 +1,55 @@
-const express = require('express');
-const mongoose = require('mongoose'); 
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+
+
 
 const registerRouter = require('./routes/LoginRegisterDashboard/registerRouter');
 const authRoutes = require('./routes/LoginRegisterDashboard/authRoutes');
 const authDashboard = require('./routes/LoginRegisterDashboard/authDashboard');
 const inventoryRoutes = require('./routes/inventory');
 
-//sanjuka - routes,models
-const itemsRouter = require('./routes/items');
-const lendedItemsRouter = require('./routes/lendedItems');
-const Item = require("./models/Item");
-const userItemListRouter = require('./routes/userItemList');
 
-const app = express();
-const port = process.env.PORT; 
+const app = express()
 
-// Middleware
+
+//middleware
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5174', credentials: true }));
-app.use(cookieParser()); 
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true
+}));
+app.use(cookieParser());
 
-// Prabashwara's Apis
+
+//Prabashwara's Apis
 app.use('/register', registerRouter);
 app.use('/', authRoutes);
 app.use('/dashboard', authDashboard);
 
-// Binura's Api
+//Binura's Api
 app.use('/inventory', inventoryRoutes);
 
 app.get('/logout', (req, res) => {
     res.clearCookie('token');
-    res.json({ Status: true });
-});
+    res.json({ Status: true })
+})
 
-// sanjuka - routes
-app.use('/items', itemsRouter); 
-app.use('/lendedItems', lendedItemsRouter);
-app.use('/userItemList', userItemListRouter);
 
-// Database connection
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//Database connection
+mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
-        console.log('Connected to db');
+        //Server setup
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to db and listening to port ', process.env.PORT)
+        })
     })
     .catch((error) => {
-        console.log(error);
-    });
+        console.log(error)
+    })
 
-app.get('/', (req, res) => {
-    res.send('Hello, this is your backend server!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`); 
-});
--- Frontend/src/Components/Dashboard/Dashboard_Header.jsx
+++ Frontend/src/Components/Dashboard/Dashboard_Header.jsx
@@ -1,88 +1,94 @@
 import React from "react";
-import {
-  BsFillBellFill,
-  BsFillEnvelopeFill,
-  BsPersonCircle,
-  BsSearch,
-  BsJustify,
-} from "react-icons/bs";
+import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify }
+    from 'react-icons/bs'
 
-import { useEffect, useState } from "react";
+
+import { useEffect, useState  } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { clearUser } from "../ReduxTool/userSlice";
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { clearUser } from '../ReduxTool/userSlice';
 
 function Header() {
-  const user = useSelector((state) => state.user.user);
+    const user = useSelector((state) => state.user.user)
 
-  if (!user) {
-    return <h2>No user informations</h2>;
-  }
+    if (!user) {
+        return <h2>No user informations</h2>
+    }
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  //variable for success msg
-  const [suc, setSuc] = useState();
+    //variable for success msg
+    const [suc, setSuc] = useState();
 
-  axios.defaults.withCredentials = true;
+    axios.defaults.withCredentials = true;
 
-  //Protected routes
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/dashboard")
-      .then((res) => {
-        if (res.data === "Success") {
-          setSuc(": Authorize Success !");
-          console.log(res.data);
-        } else {
-          navigate("/");
-          //console.log(err)
 
-          setSuc("Not Succeeded !");
-        }
-      })
-      .catch((err) => console.log(err));
-  }, []);
 
-  const dispatch = useDispatch();
+    //Protected routes
+    useEffect(() => {
+        axios.get('http://localhost:3001/dashboard')
+            .then(res => {
+                if (res.data === "Success") {
 
-  const handleLogout = () => {
-    axios.get("http://localhost:3001/logout").then((res) => {
-      if (res.data.Status) {
-        navigate("/login");
-      }
-    });
-    dispatch(clearUser());
-    navigate("/login"); // Redirect to the login page after logout
-  };
-  return (
-    <header className="header">
-      <div className="menu-icon">
-        <BsJustify className="icon" />
-      </div>
+                    setSuc(": Authorize Success !")
+                    console.log(res.data)
 
-      <div className="header-left">
-        <BsSearch className="icon" />
-      </div>
+                } else {
+                    navigate('/')
+                    //console.log(err)
 
-      <div className="header-right">
-        {/* <BsFillBellFill className="icon"/>
+                    setSuc("Not Succeeded !")
+                }
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        axios.get('http://localhost:3001/logout')
+            .then(res => {
+                if (res.data.Status) {
+                    navigate('/login')
+                }
+            })
+        dispatch(clearUser());
+        navigate('/login'); // Redirect to the login page after logout
+    };
+    return (
+
+
+        <header className="header">
+            <div className="menu-icon">
+                <BsJustify className="icon" />
+            </div>
+
+            <div className="header-left">
+                <BsSearch className="icon" />
+
+            </div>
+
+            <div className="header-right">
+                {/* <BsFillBellFill className="icon"/>
                 <BsFillEnvelopeFill className="icon"/> */}
 
-        <BsPersonCircle className="iconHeader" />
-        <span className="span">Welcome {user.name}</span>
+                <BsPersonCircle className="iconHeader" />
+                <span className="span">Welcome {user.name}</span>
+ 
+                <span className="span">
+                    <button onClick={handleLogout} className="btn btn-primary">
+                        Logout
+                    </button>
+                </span>
 
-        <span className="span">
-          <button onClick={handleLogout} className="btn btn-primary">
-            Logout
-          </button>
-        </span>
+                <h6>{suc}</h6>
 
-        <h6>{suc}</h6>
-      </div>
-    </header>
-  );
+            </div>
+
+        </header>
+    )
 }
 export default Header;
