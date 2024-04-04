d96fb3f27438e8ead415899cc6eb944a15801beb
Dilshan K B <127595699+Binura22@users.noreply.github.com>
2024-04-04 20:35:11 +0530

    errors fixed

--- Frontend/src/Components/Home/Home-ProductDetails.jsx
+++ Frontend/src/Components/Home/Home-ProductDetails.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
+import homeCss from "./home.module.css";
 
 const HomeProductDetails = ({Inventory}) => {
     return ( 
-        <div style={{ alignItems: "center", textAlign: "center", margin:"auto" }}>
+        <div className={homeCss.homeProductDetails}>
             <Link to={ `http://localhost:5173/cusSelectedItem/${Inventory._id}`}>
-            <div className="homeProductDetails" >
-                <div className="product-picture"><img src={`http://localhost:8000/images/`+Inventory.img_URL} alt="Product" style={{ maxWidth: "180px" ,height:"220px"}}/></div>
+                <div className={homeCss.productPicture}><img src={`http://localhost:8000/images/`+Inventory.img_URL} alt="Product" /></div>
                 <br></br>
                 <div><strong>{Inventory.name}</strong></div>
-                <div>Rs :{Inventory.price}</div>
-            </div>
+                <div>Rs {Inventory.price}.00</div>
             </Link>
         </div>
      );
